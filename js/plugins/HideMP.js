2af1f17891aa797858cab3a7dfd443d62fefb585
logmani <logmani@gmail.com>
2022-10-16 17:17:13 +0300

    lost which version we used :)

---
+++ js/plugins/HideMP.js
@@ -0,0 +1,14 @@
+//=============================================================================
+// HideMp.js
+//=============================================================================
+
+/*:
+ * @plugindesc HideMp.
+ * @author Sasuke KANNAZUKI
+ *
+
+ */
+
+ Window_Base.prototype.drawActorMp = function(actor, x, y, width) {
+ // NOTHING!!!
+ };
